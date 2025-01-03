d613164903cd1cbfc04775aaff8372bd69b3519c
KenzyFrhat <137256482+KenzyFrhat@users.noreply.github.com>
2025-01-03 23:33:48 +0200

    Update Email_System.py

--- Email_System.py
+++ Email_System.py
@@ -1,6 +1,6 @@
 import csv
 import re
-
+import pathlib
 
 class Users:
 
@@ -21,27 +21,32 @@ class Users:
         return re.match(pattern,email)
 
     @staticmethod
-    def check_password_complexity(password):
-        pattern = r"^(?=.[A-Z])(?=.[a-z])(?=.\d)(?=.[@$!%?&])[A-Za-z\d@$!%?&]{1,10}$"
-        return re.match(password,pattern)
+    def check_email_existence(email):
+        for user in Data.lst_users:
+            if user.email == email:
+                return True
 
     @staticmethod
     def print_password_rules():
         password_rules = '''
-                  <password rules>:
-                  one upper lette
-                  one lower letter 
-                  one special character
-                  one number
-                  Maximum length: 10'''
+                     <password rules>:
+                     one upper letter
+                     one lower letter 
+                     one special character
+                     one number
+                     Maximum length: 10'''
         print(password_rules)
 
+    @staticmethod
+    def isvalid_password(password):
+        pattern = r"^(?=.[A-Z])(?=.[a-z])(?=.\d)(?=.[@$!%?&])[A-Za-z\d@$!%?&]{1,10}$"
+        return re.match(password,pattern)
 
-    def register(self):
-        ...
-    def logging_in(self):
-        ...
-
+    @staticmethod
+    def get_password_by_email(email):
+        for user in Data.lst_users:
+           if user.email == email:
+               return user.password
 
 class Data:
     lst_users = []
@@ -49,9 +54,13 @@ class Data:
     @classmethod
     def add(cls, user):
         Data.lst_users.append(user)
-
-
     # working with csv file
+
+    @staticmethod
+    def file_is_exist(file_path):
+        file_path = pathlib.Path(file_path)
+        return file_path.exists()
+
 
     @staticmethod
     def read_file(file=r"E:\Documents\PythonProject\users.csv"):
@@ -62,18 +71,44 @@ class Data:
                 name = row["Name"]
                 email = row["Email"]
                 password = row["Password"]
-                user = Users(name, email, password, file=True)
+                Users(name, email, password, file=True)
+                print(row)
             return file_lst_users
 
     @classmethod
-    def add_file_users(cls,file_lst_users):
+    def import_file_users(cls,file_lst_users):
         for user in file_lst_users:
           cls.lst_users.append(user)
 
+    @classmethod
+    def store_data_in(cls, file=r"E:\Documents\PythonProject\users.csv"):
+        with open(file, "a", newline="") as f :
+            fieldnames = ["Name", "Email", "Password"]
+            writer = csv.DictWriter(f, fieldnames=fieldnames)
+            writer.writeheader()
+            for user in Data.lst_users:
+                user_info = {}
+                user_info["Name"] = user.name
+                user_info["Email"] = user.email
+                user_info["Password"] = user.password
+                writer.writerow(user_info)
+
 if __name__ == "__main__":
+
+    #  chhecking existence
+    file = r"E:\Documents\PythonProject\users.csv"
+    file_is_exist = Data.file_is_exist(file)
+    if not file_is_exist:
+        print("file not found")
+
+    # read data & import
+    file_users = Data.read_file(file)
+    Data.import_file_users(file_users)
+
+
     while True:
         message = '''
-        Bank system: 
+        Bank system:
         Enter your choice number:
         1- Register
         2- Log in
@@ -81,50 +116,65 @@ if __name__ == "__main__":
         print(message)
         choice = input(">>")
 
-
+        for user in Data.lst_users:
+            print(user)
         # registration
         if choice == "1":
-           #  get name
            name = input("Enter your name: ")
-
            # get email
            while True:
-               email = input("Enter your email: ")
-               if Users.email_validation(email):
-                 break
-               else:
+               inp_email = input("Enter your email (Press 0 to cancel): ")
+               if inp_email == "0": break
+               # check email validation
+               isvalid = Users.email_validation(inp_email)
+               if not isvalid:
                    print("Invalid email")
+                   continue
+               else:
+                 is_exist = Users.check_email_existence(inp_email)
+                 if is_exist:
+                   print("This email is already exist")
+                   continue
 
-           #  get password
-           while True :
-               Users.print_password_rules()
-               inp_password = input("Enter password: ")
+           # got password
+               while True :
+                   Users.print_password_rules()
+                   inp_password = input("Enter password (press 0 to cancel): ")
+                   if inp_password == "0": break
+                   # if isvalid_password(inp_password):
+                   Users(name, inp_email, inp_password)
+                   print("successfully registration !")
+                   break
                break
-               # if Users.check_password_complexity(inp_password):
-               #     break
-               # else:
-               #     print("Invalid password")
-           Users(name, email, inp_password)
-           print("successfully registration !")
-           continue
-
-
-
-
-
+                   # else:
+                   #     print("Invalid password")
         # log in
         elif choice == "2":
-            ...
+          # get email
+            while True:
+              inp_email = input("Enter your registered email (Press 0 to cancel): ")
+              if inp_email == "0": break
+              # check email existence
+              is_exist = Users.check_email_existence(inp_email)
+              if not is_exist :
+                  print("Email doesn't exist")
+                  continue
+        # got password
+              while True :
+                inp_password = input("Enter your password (Press 0 to cancel): ")
+                if inp_password == 0 : break
+            #      check password correctness
+                password = Users.get_password_by_email(inp_email)
+                if inp_password == password:
+                    print("log in successfully")
+                    break
+            break
 
         elif choice == "3":
             print("Thank you") # flushing feature
+            # saving data in a file
+            Data.store_data_in(file)
             break
+
         else:
             print("invalid input")
-
-
-
-
-
-
-
